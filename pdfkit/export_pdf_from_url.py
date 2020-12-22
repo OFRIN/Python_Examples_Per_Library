980962dd4d24fee8c2dd6a1cedd8fe403f9af7e0
OFRIN <josanghyeokn@gmail.com>
2020-12-22 17:23:41 +0900

    update export code

--- pdfkit/export_pdf_from_url.py
+++ pdfkit/export_pdf_from_url.py
@@ -6,3 +6,4 @@ config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
 # Convert google.pdf from url, which is http://google.co.kr/.
 pdfkit.from_url("http://google.co.kr", "google.pdf", configuration=config)
+# pdfkit.from_url("https://stackoverflow.com/questions/14494747/how-to-add-images-to-readme-md-on-github", "stackoverflow.pdf", configuration=config)
